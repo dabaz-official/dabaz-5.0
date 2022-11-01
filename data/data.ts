23a38b6e195a598b711a51978f752b8a4efe2266
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-11-01 18:21:27 +0800

    Create data.ts

---
+++ data/data.ts
@@ -0,0 +1,75 @@
+export const links = [
+  {
+    title: 'About',
+    href: '#about',
+  },
+  {
+    title: 'Blog',
+    href: '#blog',
+  },
+  {
+    title: 'Links',
+    href: 'https://dabazofficial.com',
+  },
+  {
+    title: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const posts = [
+  {
+    title: 'HTTP Protocol',
+    href: 'https://dabaz-learn.vercel.app/blog/http-protocol',
+    category: { name: 'Web Basics', href: 'https://dabaz-learn.vercel.app/categories/web-basics' },
+    description:
+      'HTTP is a protocol for fetching resources such as HTML documents. It is the foundation of any data exchange on the Web and it is a client-server protocol, which means requests are initiated by the recipient, usually the Web browser. A complete document is reconstructed from the different sub-documents fetched, for instance, text, layout description, images, videos, scripts, and more.',
+    date: 'Sep 17, 2022',
+    datetime: '2022-09-17',
+    imageUrl:
+      'https://dabaz-learn.vercel.app/assets/http-protocol_Z1fB4of.webp',
+    readingTime: '8 min',
+    author: {
+      name: 'DabAZ',
+      href: 'https://dabaz.me',
+      imageUrl:
+        '/dabaz.jpg',
+    },
+  },
+  {
+    title: 'Git Remote',
+    href: 'https://dabaz-learn.vercel.app/blog/git-remote',
+    category: { name: 'Dev Basics', href: 'https://dabaz-learn.vercel.app/categories/dev-basics' },
+    description:
+      'A Git workflow is a recipe or recommendation for how to use Git to accomplish work in a consistent and productive manner.',
+    date: 'Sep 16, 2022',
+    datetime: '2022-09-16',
+    imageUrl:
+      'https://dabaz-learn.vercel.app/assets/git-remote_ZGlPja.png',
+    readingTime: '12 min',
+    author: {
+      name: 'DabAZ',
+      href: 'https://dabaz.me',
+      imageUrl:
+        '/dabaz.jpg',
+    },
+  },
+  {
+    title: 'Git Basics',
+    href: 'https://dabaz-learn.vercel.app/blog/git-basics',
+    category: { name: 'Dev Basics', href: 'https://dabaz-learn.vercel.app/categories/dev-basics' },
+    description:
+      'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
+    date: 'Sep 15, 2020',
+    datetime: '2020-09-15',
+    imageUrl:
+      'https://dabaz-learn.vercel.app/assets/git-basics_1aQ4B1.png',
+    readingTime: '11 min',
+    author: {
+      name: 'DabAZ',
+      href: 'https://dabaz.me',
+      imageUrl:
+        '/dabaz.jpg',
+    },
+  },
+];
